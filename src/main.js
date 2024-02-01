68e35f20f2b9456e2d551abbfba9f51751a0d444
hojicya <hojicya01@gmail.com>
2024-02-01 21:31:41 +0900

    build setting

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './assets/main.css'
 import "@/styles/global.scss";
 
 import { createApp } from 'vue'
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { resolve } from "path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const src = resolve(__dirname, "./src/pages");
-const dist = resolve(__dirname, "./dist");
+const src = resolve(__dirname, "./src");
+// const dist = resolve(__dirname, "./dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL(src, import.meta.url)),
     },
   },
   css: {
@@ -23,6 +24,24 @@ export default defineConfig({
           @use "@/styles/base/_variables.scss" as *;
           @use "@/styles/base/_mixin.scss" as *;
           `,
+      },
+    },
+  },
+  build: {
+    minify: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name].[ext]";
+          }
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/css/[name].[ext]";
+          }
+          return "assets/[name].[ext]";
+        },
       },
     },
   },
